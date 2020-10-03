c3bd433963fb0c1741f5b8cb6ea392e0f091b727
Lucas Lima <lucas.alima@souunit.com.br>
2020-10-02 22:33:26 -0300

    sign up page

--- client/src/pages/login/index.js
+++ client/src/pages/login/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import './styles.css';
 
 function Login() {
-    return (<div className="login-container"> 
-        <div className="left-container">
+    return (<div className="sign-container"> 
+        <div className="inputs-container-sign">
             <h1>Sign in to Account</h1>
             <input type="text" placeholder="E-mail"/><br/>    
             <input type="password" placeholder="Password" /> <br/>
             <button>Sign in</button>
         </div>
-        <div className="right-container">
+        <div className="navegation-container-sign">
 
             <h1>Hello, friend!</h1>
             <a href="/signup">Sign up</a> <br/>
--- client/src/pages/sign-up/index.js
+++ client/src/pages/sign-up/index.js
@@ -2,9 +2,24 @@
 import React from 'react';
 
 function SignUp() {
-    return (<> <h1>Sign Up</h1>
-            <a href="/">Login</a>
-     </>)
+    return (<div className="sign-container"> 
+            <div className="navegation-container-sign">
+
+            <h1>Welcome back!</h1>
+            <a href="/">Sign in</a> <br/>
+
+            </div>
+
+            <div className="inputs-container-sign">
+
+            <h1>Create Account</h1>
+            <input type="text" placeholder="Name"/><br/>    
+            <input type="text" placeholder="E-mail"/><br/>    
+            <input type="password" placeholder="Password" /> <br/>
+            <input type="password" placeholder="Confirm Password" /> <br/>
+            <button>Sign up</button>
+            </div>
+     </div>)
 }
 
 export default SignUp;
