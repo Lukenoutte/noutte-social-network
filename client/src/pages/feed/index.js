71015b6f32e8f618793b48c571354007ef8af5cb
Lucas Lima <lucas.alima@souunit.com.br>
2020-10-23 18:54:11 -0300

    start feed

--- client/src/pages/feed/index.js
+++ client/src/pages/feed/index.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React from "react";
+import "../../assets/styles/feed.css";
 
 function Feed() {
-    return (<> <h1>Feed</h1>
-            <a href="/profile">Profile</a> <br/>
-            <a href="/">Logout</a>
-     </>)
+  return (
+    <div className="feed-container">
+      <header>
+        <div className="center-container-header">
+          <div className="logo">.</div>
+          <div className="icon-profile">.</div>
+        </div>
+      </header>
+      <div className="main-container">
+        <div className="center-container">
+          <h1>Feed</h1>
+          <a href="/profile">Profile</a> <br />
+          <a href="/">Logout</a>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Feed;
