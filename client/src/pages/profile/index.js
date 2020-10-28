3c13ce5e039c7fd420e455731f6d7b707e541336
Lucas Lima <lucas.alima@souunit.com.br>
2020-10-28 15:16:22 -0300

    social media

--- client/src/pages/profile/index.js
+++ client/src/pages/profile/index.js
@@ -1,12 +1,50 @@
 import React from "react";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
+import "../../assets/styles/pages/profile.css";
 
 function Profile() {
   return (
     <div className="profile-container">
       <Header />
-      <h1>Profile</h1>
-      <a href="/feed">Feed</a>
+      <div className="center-container">
+        <div className="left-container">
+          <h2>Profile</h2>
+          <div className="profile-pic-wrapper">
+            <div className="profile-picture"></div>
+            <button>+Upload new photo</button>
+          </div>
+
+          <div className="social-wrapper">
+            <p id="social-title">Social media</p>
+            <div className="social-media">
+              <div className="icon-and-name">
+                <div className="icon-social"></div>
+                <p>Social Name</p>
+              </div>
+              <button>+</button>
+            </div>
+
+            <div className="social-media">
+              <div className="icon-and-name">
+                <div className="icon-social"></div>
+                <p>Social Name</p>
+              </div>
+              <button>+</button>
+            </div>
+
+            <div className="social-media">
+              <div className="icon-and-name">
+                <div className="icon-social"></div>
+                <p>Social Name</p>
+              </div>
+              <button>+</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
